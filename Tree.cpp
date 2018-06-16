aae098f80ab9fecf038a9bb7e0583c38797c02d5
Kostiya <kostiya@users.noreply.github.com>
2018-06-16 19:32:15 +0300

    Tree Header updated.

--- Tree.cpp
+++ Tree.cpp
@@ -1,45 +1,5 @@
 #include "Tree.h"
 
 namespace structure{
-    void Tree::printTree() const{
 
-    }
-    const Tree::Node& Tree::insert(T key){
-        Node y = nullptr;
-        Node x = this->root;
-        while(x != nullptr){
-            y = x;
-            Location loc;
-            if (key < x->key){
-                loc = left;
-                x = x->left;
-            } else{
-                loc = right;
-                x->right;
-            }
-        }
-        if(y == nullptr){
-            this->root=Node(key);
-        }
-        return nullptr;
-    }
-
-    bool  Tree::remove(T key){
-        return true;
-    }
-    const Tree::Node& Tree::find(T key) const{
-        return nullptr;
-    }
-    const Tree::Node& Tree::minimum(T key) const{
-        return nullptr;
-    }
-    const Tree::Node& Tree::maximum(T key) const{
-        return nullptr;
-    }
-    const Tree::Node& Tree::succsessor(T key) const{
-        return nullptr;
-    }
-    const Tree::Node& Tree::predecessor(T key) const{
-        return nullptr;
-    }
 }
--- Tree.h
+++ Tree.h
@@ -7,43 +7,47 @@ using std::cout;
 using std::endl;
 namespace structure{
     //template <class T>
-    typedef int T;
-    class Tree{
-    public:
-        enum Location{right, left};
-        typedef class node{
-            friend class Tree;
-        protected:
-            node* p;
-            node* left;
-            node* right;
-            T key;
-        public:
-            node(T key,node* p= nullptr,node* left= nullptr,
-                    node* right= nullptr):key(key){};
-        } *Node;
-        const Node& insert(T key);
-        bool  remove(T key);
-        const Node& find(T key) const;
-        const Node& minimum(T key) const;
-        const Node& maximum(T key) const;
-        const Node& succsessor(T key) const;
-        const Node& predecessor(T key) const;
-        void printTree() const;
+    class class_node;
+    typedef class_node *Node;
 
+
+    typedef int T;
+    class class_node{
     protected:
-        Node root;
-    private:
-        void printNode(Tree::Node node, int depth=0){
-            if(node == nullptr)
-                return;
-            for(int i = 0; i < depth*4;i++)
-                cout << " ";
-            cout << node->key << endl;
-            printNode(node->left , ++depth);
-            printNode(node->right, ++depth);
-        }
+        Node p;
+        Node left;
+        Node right;
+        T key;
+    public:
+        enum Location{left, right};
+        explicit class_node(T key,Node p= nullptr,Node left= nullptr,
+                Node right= nullptr);
+        class_node()=delete;
+        class_node(class_node node) = default;
+        Node getParent() const;
+        Node getLeft() const;
+        Node getRight() const;
+        T getKey() const;
+        Node insert(T key);
+        bool remove(T key);
+        Node find(T key) const;
+        Node minimum(T key) const;
+        Node maximum(T key) const;
+        Node succsessor(T key) const;
+        Node predecessor(T key) const;
     };
+
+    void printNode(Node node, int depth=0){
+        if(node == nullptr)
+            return;
+        for(int i = 0; i < depth*4;i++)
+            cout << " ";
+        cout << node->getKey() << endl;
+        printNode(node->getLeft() , ++depth);
+        for(int i = 0; i < depth*4+10;i++)
+            cout << "-";
+        printNode(node->getRight(), ++depth);
+    }
 }
 
 #endif //BINARYTREE_TREE_H
